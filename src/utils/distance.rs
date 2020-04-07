58cd39d0eef0e2aaf0f55516dfd1b174f13e45d3
pprobst <pprobst@insiberia.net>
2020-04-07 16:03:05 -0300

    oops

--- src/utils/distance.rs
+++
@@ -1,6 +0,0 @@
-use bracket_lib::{line_2d, line_2d_bresenham}
-
-pub fn line_to(orig: Point, dest: Point, bresenham: bool) -> Vec<Point> {
-    let points;
-    if bresenham { points = line_2d_bresenham(orig, dest) }
-}
